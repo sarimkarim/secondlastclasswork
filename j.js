3ca05fce5fba9aeddcd4599b6f2b8636d9a251b4
sarimkarim <l1s22bsse0003@ucp.edu.pk>
2025-06-24 10:32:22 +0500

    Create j.js

---
+++ j.js
@@ -0,0 +1,4 @@
+document.getElementById('greetBtn').addEventListener('click', function() {
+    const message = document.getElementById('message');
+    message.textContent = "Hello! Thanks for visiting.";
+});
